a04ce0b3bfecf10d967be7302be6ed60a1562ede
Escribe tu nombre sin tildes, y letras como ø, ñ <patricio.ibarra.g@gmail.com>
2017-10-18 01:45:49 -0300

    respuestasavance3

--- 2 ciclos/ejercicio10.rb
+++ 2 ciclos/ejercicio10.rb
@@ -2,10 +2,10 @@
 # e imprimirlo reiteradamente hasta que el usuario ingrese la opción número 4.
 
 ready = 0
-while ready == 0
+while ready != 4
   puts "Opción 1: blah"
   puts "Opción 2: blah"
   puts "Opción 3: blah"
   puts "Opción 4: Salir"
-  ready = gets.chomps.to_i
+  ready = gets.chomp.to_i
 end
